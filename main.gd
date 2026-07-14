9088ee1d96322a7832d2621565af1498584d3919
kerryhalipad <kerryhalipad@gmail.com>
2026-07-14 21:07:49 +0200

    Started My Game
    imported recommended assets, set up Game Board and Markers, Set mouse clicks (made sure clicks are detected only in the grid and not out of the grid).

--- main.gd
+++ main.gd
@@ -1,12 +1,22 @@
 extends Node
-fyufy yyiooiup
+
+var board_size: int
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _ready():
+	board_size = $Board.texture.get_width()
+	print(board_size)
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	print_tree()
+func _process(delta):
 	pass
+	
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			if event.position.x <board_size:
+				print(event.position)
+
+	
